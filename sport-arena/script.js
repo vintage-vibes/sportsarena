cfbb205d4f2cb5098d0b7a426757cabbd494ad90
vintage-vibes <sajimusa5@gmail.com>
2025-11-18 16:00:09 +0200

    Delete sport-arena directory

--- sport-arena/script.js
+++
@@ -1,7 +0,0 @@
-const menutoggle = document.getElementById('menu-toggle');
-const navLinks = document.querySelector('.navbar ul')
-
-menutoggle.addEventListener("click" ,()=>{
-    navLinks.classList.toggle('active');
-    menutoggle.classList.toggle("open");
-})
